e83d5d897b96d1a1bbf824daf72d0b58f0f8af92
tl2120 <terran-wei-yang.loh20@imperial.ac.uk>
2021-03-27 15:02:09 +0000

    initial setup

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Challenges, Status
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Challenges)
+admin.site.register(Status)
